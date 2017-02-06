5d15b3c8596a35b2f3820c0892a49b8f7896c766
Scrivener07 <ScrivenerMods@gmail.com>
2017-02-06 04:43:19 -0800

    added a command for counting the current selection

--- src/extension.ts
+++ src/extension.ts
@@ -3,27 +3,48 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
+export function activate(context: vscode.ExtensionContext)
+{
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "papyrus-code" is now active!');
 
+
+
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let papyrusHello = vscode.commands.registerCommand('extension.papyrusHello', () =>
+    {
         // The code you place here will be executed every time your command is executed
-
         // Display a message box to the user
         vscode.window.showInformationMessage('Hello World!');
     });
+    context.subscriptions.push(papyrusHello);
 
-    context.subscriptions.push(disposable);
+
+    let papyrusCount = vscode.commands.registerCommand('extension.papyrusCount', () =>
+    {
+        var editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
+
+        var selection = editor.selection;
+        var text = editor.document.getText(selection);
+
+        // Display a message box to the user
+        vscode.window.showInformationMessage('Selected characters: ' + text.length);
+    });
+    context.subscriptions.push(papyrusCount);
+
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
+export function deactivate()
+{
 }
