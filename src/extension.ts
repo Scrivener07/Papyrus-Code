6ab27fdb172eea5a617aace2319eb80883414faf
Scrivener07 <ScrivenerMods@gmail.com>
2017-02-06 07:14:49 -0800

    fixed the statusbar word counter
    -these changes are part of VS Code documentation.
    https://code.visualstudio.com/docs/extensions/example-word-count

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,35 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import {window, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} from 'vscode';
+import {
+    window,
+    workspace,
+    commands,
+    Disposable,
+    ExtensionContext,
+    StatusBarAlignment,
+    StatusBarItem,
+    TextDocument
+} from 'vscode';
 
 
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
     console.log('The extension "papyrus-code" is now active.');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
+    let wordCounter = new WordCounter();
+    let controller = new WordCounterController(wordCounter);
+    context.subscriptions.push(controller);
+    context.subscriptions.push(wordCounter);
+
+
     let papyrusHello = commands.registerCommand('extension.papyrusHello', () => {
-         console.log('Command papyrusHello');
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
+        console.log('Command papyrusHello');
         window.showInformationMessage('Hello World!');
     });
     context.subscriptions.push(papyrusHello);
 
 
     let papyrusCount = commands.registerCommand('extension.papyrusCount', () => {
-         console.log('Command papyrusCount');
+        console.log('Command papyrusCount');
         var editor = window.activeTextEditor;
         if (!editor) {
             return; // No open text editor
@@ -32,38 +37,24 @@ export function activate(context: ExtensionContext) {
 
         var selection = editor.selection;
         var text = editor.document.getText(selection);
-
-        // Display a message box to the user
         window.showInformationMessage('Selected characters: ' + text.length);
     });
     context.subscriptions.push(papyrusCount);
-
-
-    // create a new word counter
-    let wordCounter = new WordCounter();
-    var statusCounter = commands.registerCommand('extension.statusCounter', () => {
-        console.log('Command statusCounter');
-        wordCounter.updateWordCount();
-    });
-    // Add to a list of disposables which are disposed when this extension is deactivated.
-    context.subscriptions.push(wordCounter);
-    context.subscriptions.push(statusCounter);
-
 }
 
-// this method is called when your extension is deactivated
+
 export function deactivate() {
     console.log('The extension "papyrus-code" is now deactivated.');
 }
 
 
 
-class WordCounter {
+export class WordCounter {
     private _statusBarItem: StatusBarItem;
 
 
     public updateWordCount() {
-
+        console.log('WordCounter.updateWordCount');
         // Create as needed
         if (!this._statusBarItem) {
             this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
@@ -78,12 +69,12 @@ class WordCounter {
 
         let doc = editor.document;
 
-        // Only update status if an Papyrus file
-        if (doc.languageId === "papyrus") {
+        // Only update status if an MD file
+        if (doc.languageId === "markdown") {
             let wordCount = this._getWordCount(doc);
 
             // Update the status bar
-            this._statusBarItem.text = wordCount !== 1 ? `${wordCount} Words` : '1 Word';
+            this._statusBarItem.text = wordCount !== 1 ? `$(pencil) ${wordCount} Words` : '$(pencil) 1 Word';
             this._statusBarItem.show();
         } else {
             this._statusBarItem.hide();
@@ -92,7 +83,7 @@ class WordCounter {
 
 
     public _getWordCount(doc: TextDocument): number {
-
+        console.log('WordCounter._getWordCount');
         let docContent = doc.getText();
 
         // Parse out unwanted whitespace so the split is accurate
@@ -107,7 +98,44 @@ class WordCounter {
     }
 
 
-    dispose() {
+    public dispose() {
+        console.log('WordCounter.dispose');
         this._statusBarItem.dispose();
     }
+
+}
+
+
+
+class WordCounterController {
+    private _wordCounter: WordCounter;
+    private _disposable: Disposable;
+
+
+    constructor(wordCounter: WordCounter) {
+        console.log('WordCounterController.constructor');
+        this._wordCounter = wordCounter;
+        this._wordCounter.updateWordCount();
+
+        // subscribe to selection change and editor activation events
+        let subscriptions: Disposable[] = [];
+        window.onDidChangeTextEditorSelection(this._onEvent, this, subscriptions);
+        window.onDidChangeActiveTextEditor(this._onEvent, this, subscriptions);
+
+        // create a combined disposable from both event subscriptions
+        this._disposable = Disposable.from(...subscriptions);
+    }
+
+
+    private _onEvent() {
+        console.log('WordCounterController._onEvent');
+        this._wordCounter.updateWordCount();
+    }
+
+
+    public dispose() {
+        console.log('WordCounterController.dispose');
+        this._disposable.dispose();
+    }
+
 }
