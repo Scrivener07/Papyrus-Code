92f565a5931f93c498140eff6a965804487ed495
Scrivener07 <ScrivenerMods@gmail.com>
2017-02-09 19:37:15 -0800

    removed unused import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
 import * as vscode from 'vscode';
 import { window, workspace, commands, ExtensionContext } from 'vscode';
 import { PapyrusHelloFeature } from './features/papyrusHello';
-import { PapyrusWordCounter, PapyrusWordCounterController, PapyrusWordCountFeature } from './features/papyrusCount';
+import { PapyrusWordCountFeature } from './features/papyrusCount';
 import { PapyrusPreviewFeature } from './features/papyrusPreview';
 import { PapyrusCompileFeature } from './features/papyrusCompile';
 
