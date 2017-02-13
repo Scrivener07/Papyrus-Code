05565810d525f71a627758036db4f032803914f5
Scrivener07 <ScrivenerMods@gmail.com>
2017-02-12 17:22:09 -0800

    testing csharp like dispose

---
+++ src/common/system.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+export interface IDisposable {
+    dispose();
+}
+
+export function using<T extends IDisposable>(resource: T, func: (resource: T) => void) {
+    try {
+        func(resource);
+    } finally {
+        resource.dispose();
+    }
+}
